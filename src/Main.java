7c79b915442cf8bc818549a2981f88b7aba5299d
g21turbo <dg21stew@gmail.com>
2023-10-17 22:03:48 -0700

    added another method canBuyAlcohol2

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,12 @@ public class Main {
         String grade = getGrade(75);
         System.out.println("The grade is: " + grade);
 
-        boolean age = canBuyAlcohol(19);
+        boolean age = canBuyAlcohol(20);
         System.out.println("This person can buy alcohol: " + age);
+
+        boolean age2 = canBuyAlcohol2(21);
+        System.out.println("This person can buy alcohol: " + age2);
+
     }
 
     public static int add(int a, int b) {
@@ -30,5 +34,13 @@ public class Main {
             return false;
         }
         return true;
+    }
+
+    public static boolean canBuyAlcohol2(int age) {
+        boolean result = true;
+        if (age < 21){
+            return false;
+        }
+        return result;
     }
 }
